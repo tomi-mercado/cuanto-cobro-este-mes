79baf58f810f2c4addccd91b6bc7132faa3f6af2
Tomas <tmercadoslp@gmail.com>
2024-01-26 14:26:26 -0300

    feat: header component include a link to home

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -1,21 +1,26 @@
 import Image from "next/image";
+import Link from "next/link";
 import React from "react";
 
 const Header: React.FC = () => {
   return (
-    <header className="container flex py-4 gap-4 items-center">
-      <div>
-        <Image
-          src="https://aerolab.co/static/images/favicon-167.png"
-          alt="Aerolab logo"
-          width={36}
-          height={36}
-        />
-      </div>
-      <div className="flex flex-col">
-        <h1 className="text-xl sm:text-2xl font-bold">Cuánto Aerocobro</h1>
-        <p className="text-xs sm:text-sm">Mi sueldo en ARS este mes en Aero</p>
-      </div>
+    <header className="container py-4">
+      <Link href="/" className="flex items-center gap-4">
+        <div>
+          <Image
+            src="https://aerolab.co/static/images/favicon-167.png"
+            alt="Aerolab logo"
+            width={36}
+            height={36}
+          />
+        </div>
+        <div className="flex flex-col">
+          <h1 className="text-xl sm:text-2xl font-bold">Cuánto Aerocobro</h1>
+          <p className="text-xs sm:text-sm">
+            Mi sueldo en ARS este mes en Aero
+          </p>
+        </div>
+      </Link>
     </header>
   );
 };
