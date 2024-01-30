ac38379451acb95b8c08979c614b7f8952cdf7ec
Tomas <tmercadoslp@gmail.com>
2024-01-30 09:49:55 -0300

    feat: favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -9,6 +9,12 @@ const inter = Inter({ subsets: ["latin"] });
 export const metadata: Metadata = {
   title: "Cuánto cobro",
   description: "Mi sueldo en ARS este mes",
+  icons: [
+    {
+      rel: "icon",
+      url: "data:image/svg+xml,<svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 100 100'><text y='.9em' font-size='90'>🪁</text></svg>",
+    },
+  ],
 };
 
 export default function RootLayout({
